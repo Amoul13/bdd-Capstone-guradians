e4737874f9ecc6e4a8d2693cddc09022ffafeb44
Amal <amal_brahimi@yahoo.com>
2023-09-02 16:42:25 -0400

    update firefox

--- src/main/java/tek/bdd/guardians/config/FireFoxBrowser.java
+++ src/main/java/tek/bdd/guardians/config/FireFoxBrowser.java
@@ -8,15 +8,31 @@ import io.github.bonigarcia.wdm.WebDriverManager;
 
 public class FireFoxBrowser implements Browser{
 
-	@SuppressWarnings("deprecation")
-	@Override
-	public WebDriver openBrowser(String url) {
-		WebDriverManager.firefoxdriver().setup();
-        FirefoxOptions options = new FirefoxOptions();
-        options.setHeadless(true);
-        WebDriver driver = new FirefoxDriver();
-        driver.get(url);
-        return driver;
+//	@SuppressWarnings("deprecation")
+//	@Override
+//	public WebDriver openBrowser(String url) {
+//		WebDriverManager.firefoxdriver().setup();
+//        FirefoxOptions options = new FirefoxOptions();
+//        options.setHeadless(true);
+//        WebDriver driver = new FirefoxDriver();
+//        driver.get(url);
+//        return driver;
+//        
+        public WebDriver openBrowser(String url) {
+
+        	WebDriverManager.firefoxdriver().setup();
+
+        	FirefoxOptions options = new FirefoxOptions();
+
+        	options.setHeadless(true);
+
+        	WebDriver driver = new FirefoxDriver(options);
+
+        	driver.get(url);
+
+        	return driver;
+
+        	
 	}
 
 }
